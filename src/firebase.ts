71f3748af8cac93655f7a8a56a80241adf6b2450
Jstngr <justi93smail@gmail.com>
2026-02-21 23:05:40 +0100

    Add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyBfMmiKqJhZOT11d16lme4yXuUiI4R9KrU",
+  authDomain: "konfettikiste-f30e2.firebaseapp.com",
+  projectId: "konfettikiste-f30e2",
+  storageBucket: "konfettikiste-f30e2.firebasestorage.app",
+  messagingSenderId: "774753797769",
+  appId: "1:774753797769:web:26f15af5a58ea555dba858",
+  measurementId: "G-19JF1PC0QW",
+};
